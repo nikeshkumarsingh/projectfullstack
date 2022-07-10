fcaeab00ce7fd8ad534313b5c1eef3e659a93d1c
nikeshkumar <nikeshmasai1947@gmail.com>
2022-07-10 23:18:04 +0530

    update with navbar and map idea

---
+++ src/components/nav.jsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Input
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box>Logo</Box>
+          <Box><Input  type="text" placeholder="search city name"/></Box>
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
